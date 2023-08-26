b5176a7016b26988549a6ed8b07bac1033692187
Daria Platonova <dplatonova@ibitcy.com>
2023-08-26 14:48:10 +0300

    added theme and home initial

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { ThemeProvider } from "@mui/material/styles";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+import App from "./App.tsx";
+
+import theme from './theme';
+
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
+  </React.StrictMode>
+);
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,7 +1,31 @@
-const Home = () => {
+import { TextField, Button } from '@mui/material';
+import { useState } from 'react';
+
+const Home: React.FC = () => {
+  const [data, setData] = useState({ change: [''], from: '' })
+  const handleSubmit = () => {}
+  
   return (
-    <div>Home</div>
-  )
-}
+    <div>
+      <h1>Search Form</h1>
+      <form onSubmit={handleSubmit}>
+        <TextField
+          label="From"
+          value={data}
+          onChange={e => setData({ ...data, from: e.target.value })}
+          required
+        />
+        <TextField
+          label="Change cities"
+          value={data.change.join(', ')}
+          onChange={e => setData({ ...data, change: e.target.value.split(', ') })}
+        />
+        <Button variant="contained" color="primary" type="submit">
+          Search
+        </Button>
+      </form>
+    </div>
+  );
+};
 
 export default Home;
