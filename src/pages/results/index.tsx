fea709b3c4d998eb2df16bad43a3186b732a1bed
Daria Platonova <dplatonova@ibitcy.com>
2023-08-27 18:26:14 +0300

    added loading to results page

--- src/pages/results/index.tsx
+++ src/pages/results/index.tsx
@@ -1,34 +1,50 @@
-import React, { useEffect, useState } from 'react';
-import { useSearchParams } from 'react-router-dom';
-import { Container, Paper, Typography, List, ListItem, ListItemText } from '@mui/material';
-import { calculateDistance, Distance } from '@/services/api';
+import React, { useEffect, useState } from "react";
+import { useSearchParams } from "react-router-dom";
+import {
+  Container,
+  Paper,
+  Typography,
+  List,
+  ListItem,
+  ListItemText,
+} from "@mui/material";
+import { calculateDistance, Distance } from "@/services/api";
 
 const Results: React.FC = () => {
   const [searchParams] = useSearchParams();
   const queryParams = Object.fromEntries(searchParams.entries());
 
-  const [resultData, setResultData] = useState<{
-    totalDistance: number;
-    distances: Distance[];
-  } | null>(null);
+  const [resultData, setResultData] = useState<
+    | {
+        totalDistance: number;
+        distances: Distance[];
+      }
+    | null
+    | Record<string, never>
+  >({});
 
   useEffect(() => {
     const selectedCities = queryParams.changes
-      ? [queryParams.from, ...queryParams.changes.split(','), queryParams.to]
+      ? [queryParams.from, ...queryParams.changes.split(","), queryParams.to]
       : [queryParams.from, queryParams.to];
 
     calculateDistance(selectedCities)
-      .then(data => setResultData(data))
-      .catch(error => {
+      .then((data) => setResultData(data))
+      .catch((error) => {
         console.error(error);
         setResultData(null);
       });
   }, [queryParams]);
 
   return (
-    <Container component={Paper} maxWidth="md" style={{ padding: '20px' }}>
+    <Container component={Paper} maxWidth="md" style={{ padding: "20px" }}>
       <Typography variant="h4">Search Results</Typography>
-      {resultData ? (
+      {!resultData ? (
+        <Typography variant="body1">
+          An error occurred while calculating distances.
+        </Typography>
+      ) : null}
+      {resultData && Object.keys(resultData).length ? (
         <div>
           <Typography variant="h6">Search Criteria:</Typography>
           <List>
@@ -37,7 +53,9 @@ const Results: React.FC = () => {
             </ListItem>
             {queryParams.changes && (
               <ListItem>
-                <ListItemText primary={`Change Cities: ${queryParams.changes}`} />
+                <ListItemText
+                  primary={`Change Cities: ${queryParams.changes}`}
+                />
               </ListItem>
             )}
             <ListItem>
@@ -47,7 +65,9 @@ const Results: React.FC = () => {
               <ListItemText primary={`Date of Trip: ${queryParams.date}`} />
             </ListItem>
             <ListItem>
-              <ListItemText primary={`Number of Passengers: ${queryParams.passengers}`} />
+              <ListItemText
+                primary={`Number of Passengers: ${queryParams.passengers}`}
+              />
             </ListItem>
           </List>
           <Typography variant="h6">Distances:</Typography>
@@ -55,17 +75,23 @@ const Results: React.FC = () => {
             {resultData.distances.map((distance, index) => (
               <ListItem key={index}>
                 <ListItemText
-                  primary={`Distance from ${distance.from} to ${distance.to}: ${distance.distance.toFixed(2)} km`}
+                  primary={`Distance from ${distance.from} to ${
+                    distance.to
+                  }: ${distance.distance.toFixed(2)} km`}
                 />
               </ListItem>
             ))}
             <ListItem>
-              <ListItemText primary={`Total Distance: ${resultData.totalDistance.toFixed(2)} km`} />
+              <ListItemText
+                primary={`Total Distance: ${resultData.totalDistance.toFixed(
+                  2
+                )} km`}
+              />
             </ListItem>
           </List>
         </div>
       ) : (
-        <Typography variant="body1">An error occurred while calculating distances.</Typography>
+        "loading..."
       )}
     </Container>
   );
