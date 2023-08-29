8172c39d14b1af23ebde781825398dfa386244ef
Daria Platonova <dplatonova@ibitcy.com>
2023-08-29 11:40:15 +0300

    into chunks

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig, loadEnv } from "vite";
+import { defineConfig, loadEnv, splitVendorChunkPlugin } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 import svgr from 'vite-plugin-svgr';
@@ -8,7 +8,7 @@ export default defineConfig(({ mode }) => {
   process.env = { ...process.env, ...loadEnv(mode, process.cwd(), "") };
 
   return {
-    plugins: [react(), svgr()],
+    plugins: [react(), svgr(), splitVendorChunkPlugin()],
     resolve: {
       alias: { "@": path.resolve(__dirname, "src/") },
     },
@@ -16,6 +16,15 @@ export default defineConfig(({ mode }) => {
       terserOptions: {
         compress: { drop_console: process.env.NODE_ENV === "production" },
       },
+      rollupOptions: {
+        output:{
+            manualChunks(id) {
+                if (id.includes('node_modules')) {
+                    return id.toString().split('node_modules/')[1].split('/')[0].toString();
+                }
+            }
+        }
+    },
       // sourcemap: process.env.NODE_ENV !== "production",
       minify: "terser",
     },
